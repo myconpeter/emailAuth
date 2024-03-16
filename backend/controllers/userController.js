63d4d96019b8f6a5bf246ad0c05876c309eee443
myconpeter <79177019+myconpeter@users.noreply.github.com>
2024-03-16 11:58:46 +0100

    ok

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -4,26 +4,30 @@ import asyncHandler from 'express-async-handler'
 import generateToken from './generateToken.js'
 
 
-import nodemailer from 'nodemailer'
-import { v4 as uuidv4 } from 'uuid';
+import nodemailer from 'nodemailer';
+import { config } from 'dotenv'; // Assuming you're using dotenv for environment variables
 
+// Load environment variables from a .env file
+config();
 
+// Create a transporter using Gmail SMTP
 let transporter = nodemailer.createTransport({
     service: "gmail",
     auth: {
         user: process.env.AUTH_EMAIL,
-        password: process.env.AUTH_PASSWORD
+        pass: process.env.AUTH_PASSWORD
     }
-})
+});
 
+// Verify transporter setup
 transporter.verify((error, success) => {
     if (error) {
-        console.log(error)
+        console.error('Transporter verification error:', error);
     } else {
-        console.log('ready')
-        console.log('success')
+        console.log('Transporter ready to send emails');
     }
-})
+});
+
 
 
 
